f85050ff4c314526045470b5b78b61f349e86569
olgachertkova <olga@chertkov.info>
2021-05-20 19:13:36 +0300

    Added DataProvider

--- src/test/java/tests/LoginPageTests.java
+++ src/test/java/tests/LoginPageTests.java
@@ -1,4 +1,28 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.util.DataProviders;
+
 public class LoginPageTests extends TestBase{
+    String password = "Qwe123123";
+
+    @Test(dataProviderClass = DataProviders.class, dataProvider = "usingCSVFile")
+    public void registerNeg(String email) throws InterruptedException {
+        WebElement loginLink = driver.findElement(By.id("login_link"));
+        loginLink.click();
+        Thread.sleep(3000);
+        WebElement emailField = driver.findElement(By.id("id_registration-email"));
+        emailField.sendKeys(email);
+        WebElement passwordField = driver.findElement(By.id("id_registration-password1"));
+        passwordField.sendKeys(password);
+        WebElement confirmPasswordField = driver.findElement(By.id("id_registration-password2"));
+        confirmPasswordField.sendKeys(password);
+        WebElement regButton = driver.findElement(By.cssSelector("button[value=\"Register\"]"));
+        regButton.click();
+        Assert.assertTrue(driver.findElement(By.className("error-block")).isDisplayed());
+
+    }
 }
---
+++ src/test/java/tests/util/DataProviders.java
@@ -0,0 +1,28 @@
+package tests.util;
+
+import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DataProviders {
+    @DataProvider
+    public static Iterator<Object[]> usingCSVFile(String path) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/email.csv")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(","));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+
+}
